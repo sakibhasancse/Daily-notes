4011000550ffed19bc5eb97eb9679d9294b65dbf
SakibHasanCse <sakibhasan9641@gmail.com>
2022-02-23 01:44:19 +0600

    Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import AddTodo from './components/AddTodo'
+import TodoItem from './components/TodoItem'
 import { getTodos, addTodo, updateTodo, deleteTodo } from './helpers/api'
 
 const App = () => {
@@ -14,6 +15,53 @@ const App = () => {
     .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
     .catch((err: Error) => console.log(err))
   }
+  
+  const handleUpdateTodo = (todo: ITodo): void => {
+     updateTodo(todo)
+    .then(({ status, data }) => {
+      if (status !== 200) {
+        throw new Error("Error! Todo not updated")
+      }
+      setTodos(data.todos)
+    })
+    .catch(err => console.log(err))
+  }
+
+  const handleDeleteTodo = (_id: string): void => {
+  deleteTodo(_id)
+    .then(({ status, data }) => {
+      if (status !== 200) {
+        throw new Error("Error! Todo not deleted")
+      }
+      setTodos(data.todos)
+    })
+    .catch(err => console.log(err))
+  }
+  
+  const handleSaveTodo = (e: React.FormEvent, formData:ITodo): void=> {
+    e.preventDefault()
+    addTodo(formData)
+      .then(({ status, data }) => {
+        if (status !== 201) {
+          throw new Error("Error! Todo not saved")
+        }
+        setTodos(data.todos)
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (<main className='App'>
+      <h1>My Todos</h1>
+      <AddTodo saveTodo={handleSaveTodo} />
+      {todos.map((todo: ITodo) => (
+        <TodoItem
+          key={todo._id}
+          updateTodo={handleUpdateTodo}
+          deleteTodo={handleDeleteTodo}
+          todo={todo}
+        />
+      ))}
+    </main>)
 
 }
 
